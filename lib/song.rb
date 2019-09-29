19913f23196a8fcbbe967202e24cca739ee5d6ad
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-29 21:47:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,18 @@
 class Song
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def new_by_filename
+    
+end
