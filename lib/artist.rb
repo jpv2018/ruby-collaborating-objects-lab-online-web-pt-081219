1f461ceb6cc7d3fa135603e0c32568f2c4c7b87b
Jason Viola <jason.viola@mavs.uta.edu>
2019-09-29 21:39:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,8 +24,15 @@ class Artist
   def self.find(name)
     @@all.detect {|artist_name| artist_name.name == name} 
   end
+  
+  def self.create(name)
+    Artist.new(name)
+  end
     
   def find_or_create_by_name(name)
-    
+      if self.find(name)
+        self.name
+      else self.create(name)
+    end
   end
 end
